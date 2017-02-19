f39fe47f5e2d1274759bf4e75ebf571656b390dc
kfonseca <karstenfonseca@gmail.com>
2017-02-18 16:13:49 -0800

    Third

--- app/src/main/java/giraffe/fonsecakarsten/com/giraffe/FetchWatson.java
+++ app/src/main/java/giraffe/fonsecakarsten/com/giraffe/FetchWatson.java
@@ -2,17 +2,12 @@ package giraffe.fonsecakarsten.com.giraffe;
 
 import android.content.Context;
 import android.os.AsyncTask;
-import android.widget.Toast;
 
 import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifyImagesOptions;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.model.VisualClassification;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.URL;
 
 /**
@@ -21,10 +16,10 @@ import java.net.URL;
 
 public class FetchWatson extends AsyncTask<URL, Integer, Long> {
 
-    private Context context;
+    //private Context context;
 
     FetchWatson(Context current) {
-        this.context = current;
+        //this.context = current;
     }
 
     @Override
@@ -32,15 +27,10 @@ public class FetchWatson extends AsyncTask<URL, Integer, Long> {
         VisualRecognition service = new VisualRecognition(VisualRecognition.VERSION_DATE_2016_05_20);
         service.setApiKey("{bd8ca3f2243490e58d837849484862d2ba777df6}");
 
-        //Context context = ();
-        CharSequence text = "Hello toast!";
-        int duration = Toast.LENGTH_SHORT;
+        File f = new File("src/main/res/mipmap/ic_launcher.png");
 
-//        Toast toast = Toast.makeText(context, text, duration);
-//        toast.show();
-        File f = new File("src/main/res/drawable/frog1");
         ClassifyImagesOptions options = new ClassifyImagesOptions.Builder()
-                .images(f)
+                .images(new File("src/main/res/mipmap/ic_launcher.png"))
                 .build();
         VisualClassification result = service.classify(options).execute();
         System.out.println(result);
